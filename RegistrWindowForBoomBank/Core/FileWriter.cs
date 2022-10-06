4aa8ce983fbe8d08f724ac09a9cc3190aa705c97
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2022-10-06 16:36:43 +0300

    Add files via upload

---
+++ RegistrWindowForBoomBank/Core/FileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
+using RegistrWindowForBoomBank.Model;
+
+namespace RegistrWindowForBoomBank.Core
+{
+    public static class FileWriter
+    {
+        //public ObservableCollection<string>
+        public static void FileWrite(User user)
+        {
+            using (StreamWriter reader = new StreamWriter(@"..\..\File\Users.txt"))
+            {
+                string userInfo = user.FirstName + ";" + user.MiddleName + ";" + user.LastName + ";" + user.Phone + ";" + user.Password + ";Клиент;" + user.BankCard;
+                reader.WriteLine(userInfo);
+            }
+        }
+    }
+}
---
+++ RegistrWindowForBoomBank/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using RegistrWindowForBoomBank.Core;
+using RegistrWindowForBoomBank.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RegistrWindowForBoomBank.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private User _user;
+        public User User
+        {
+            get => _user;
+            set
+            {
+                _user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+        public void Exit()
+        {
+            foreach (Window item  in Application.Current.Windows)
+            {
+                if(item is MainWindow)
+                {
+                    item.Close();
+                }
+            }
+        }
+        public void Reagistr()
+        {
+            if (!string.IsNullOrEmpty(User.FirstName) && !string.IsNullOrEmpty(User.MiddleName) && !string.IsNullOrEmpty(User.LastName) && !string.IsNullOrEmpty(User.Phone) && !string.IsNullOrEmpty(User.Password) && !string.IsNullOrEmpty(User.BankCard))
+            {
+                FileWriter.FileWrite(User);
+                MessageBox.Show("Вы успешно зарегестрировались!", "Регистрация", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("Одно или несколько полей пустые!", "Ошибка..", MessageBoxButton.OK);
+            }
+        }
+        public MainWindowViewModel()
+        {
+            User = new User();
+        }
+    }
+}
